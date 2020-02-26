83a328dcb7eb6e99295a2087d12e29e09ff62b95
rudxde <kj4derechte@gmail.com>
2020-02-26 18:16:40 +0100

    add title service

--- projects/cms/src/lib/components/page/page.component.ts
+++ projects/cms/src/lib/components/page/page.component.ts
@@ -1,10 +1,11 @@
 import { Component, OnInit } from '@angular/core';
-import { Router, ActivatedRoute, NavigationEnd } from '@angular/router';
+import { ActivatedRoute } from '@angular/router';
 import { IPayload } from '../../payload';
 import { ContentService } from '../../services/content.service';
-import { Location } from '@angular/common';
 import { Observable, of } from 'rxjs';
-import { map, filter, take, mergeMap } from 'rxjs/operators';
+import { map, take } from 'rxjs/operators';
+import { TitleService } from '@akrons/core';
+import { IPage } from '../../page';
 
 @Component({
   selector: 'cms-page',
@@ -18,17 +19,23 @@ export class PageComponent implements OnInit {
   constructor(
     private contentService: ContentService,
     private route: ActivatedRoute,
+    private titleService: TitleService
   ) { }
 
   ngOnInit(): void {
     const activatedRouteSnapshot = this.route.snapshot;
     if (activatedRouteSnapshot.data['template']) {
-      this.elements = of(activatedRouteSnapshot.data['template']);
+      const templatePage: IPage = activatedRouteSnapshot.data['template'];
+      this.elements = of(templatePage.elements);
+      this.titleService.setTitle(templatePage.title);
       return;
     }
     const route = activatedRouteSnapshot.url.map(x => x.toString()).join('/');
     const page = this.contentService.loadPage(route);
     this.elements = page.pipe(map(x => x.elements));
+    page.pipe(take(1)).subscribe({next: x => {
+      this.titleService.setTitle(x.title);
+    }})
   }
 
 }
